ec03940c2a6e40e4e5853cd36d01a860cf424a22
cosmicnet <webmaster@cosmicperl.com>
2011-09-30 12:42:36 +0100

    Updated version number and change log

--- lib/Cosmic/DB.pm
+++ lib/Cosmic/DB.pm
@@ -8,7 +8,7 @@ use Cosmic::DB::SQL;
 
 BEGIN {
     use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION     = '0.01_02';
+    $VERSION     = '0.01_03';
 }#BEGIN
 
 my %params_default = (
